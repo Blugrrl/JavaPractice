3dc435d027d7f5b6c63a637ac43df5e3c1bcda6d
Blugrrl <brunalsweden@gmail.com>
2016-10-28 16:32:25 +0200

    Edits

--- RandomGame/src/MyFirstGameInJava.java
+++ RandomGame/src/MyFirstGameInJava.java
@@ -4,7 +4,7 @@ public class MyFirstGameInJava {
 
 	public static void main(String[] args) {
 
-		System.out.println("Greetings kind sir / mam! Please enter your name to start the adventure");
+		System.out.println("Greetings kind sir / madam! Please enter your name to start the adventure");
 		Scanner Username = new Scanner(System.in);
 		String name = Username.nextLine();
 		System.out.println("Greetings " + name);
@@ -13,14 +13,14 @@ public class MyFirstGameInJava {
 				"You find yourself on a deserted island in front of some trees, you can either hit the tree, or walk away");
 
 		String chooseFirst = Username.nextLine();
+		String firstOption = chooseFirst;
+		switch (chooseFirst.toLowerCase()) {
 
-		switch (chooseFirst) {
-
-		case "Hit tree":
+		case "hit tree":
 			System.out.println("A mysterious fruit falls from the tree");
 			System.out.println("You can either eat the fruit or throw it");
 			break;
-		case "Walk away":
+		case "walk away":
 			System.out.println("You walk for some time and find a village. Maybe it is not so deserted!");
 			System.out.println(
 					"The village appears empty, you can either yell to see if anybody answers, or you can keep walking");
@@ -31,29 +31,29 @@ public class MyFirstGameInJava {
 
 		String chooseSecond = Username.nextLine();
 
-		switch (chooseSecond) {
-
-		case "Eat fruit":
+		switch (chooseSecond.toLowerCase()) {
+		case "eat": case "1": case "e":
+		case "eat fruit":
 			System.out.println("You eat the fruit and feel bad");
 			System.out.println("You start feeling too weak, and lie down. You die..");
 			break;
-		case "Throw fruit ":
+		case "throw fruit":
 			System.out.println("By throwing the coconut, you awaken a tiger and he eats you");
 			System.out.println("You are dead");
 			break;
-		case "Scream":
+		case "scream":
 			System.out.println("As soon as you scream, a man shoots you down from a window from one of the houses");
 			System.out.println("You died...");
 			break;
-		case "Walk":
+		case "walk":
 			System.out.println("You walked through the village, and you find a boat and leave the island");
 			System.out.println("You win!");
 			break;
 		default:
-			System.out.print("Nothing happend");
+			System.out.print("Nothing happened");
 
 		}
-
+		System.out.println("The end! Hope you enjoyed your adventure!!");
 	}
 
 }
