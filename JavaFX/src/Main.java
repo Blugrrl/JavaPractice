7d7a5fe6fd6a0e16bb8b8b7d0f54f31e4a1efe4b
Blugrrl <brunalsweden@gmail.com>
2016-11-10 15:38:09 +0100

    Java FX

--- JavaFX/src/Main.java
+++ JavaFX/src/Main.java
@@ -1,13 +1,16 @@
 import javafx.application.Application;
+import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	
+	Stage window;
 	Button button;
 	
 	public static void main(String[] args) {
@@ -17,11 +20,25 @@ public class Main extends Application {
 	}
 	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Title of the window");
+	public void start(Stage primaryStage) {
+		window = primaryStage;
+		window.setTitle("Title: ");
 		
-		button = new Button();
-		button.setText("Click me");
-	
+		button = new Button("Click me");
+		
+		button.setOnAction(e -> {
+		boolean result = ConfirmBox.display("Title of window", "Are you sure you want to do this?");
+		System.out.println(result);
+			
+		});
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		Scene scene = new Scene(layout, 300, 250);
+		window.setScene(scene);
+		window.show();
+		
 	}
+
+	
+	
 }
